ada4e527f9feb632bc582d7c1d3f846b52e0d1c7
Oliver Wise <owise1@gmail.com>
2015-06-18 16:35:51 -0700

    catch all apple captive tests

--- liblibd/captive-portal.js
+++ liblibd/captive-portal.js
@@ -1,19 +1,25 @@
 // @param httpRequest
 // @return bool - Is this a captive portal detection request
+var R = require('ramda')
 
 module.exports = function (req) {
   var ok = false 
-
+  
   function headerTester (headerAttr) {
     return function (regex) {
       if (req.headers[headerAttr] && regex.test(req.headers[headerAttr])) ok = true 
     }
   }
   // user-agents
-  [/CaptiveNetworkSupport/].forEach(headerTester('user-agent'));
+  [
+    /CaptiveNetworkSupport/
+  ].forEach(headerTester('user-agent'));
 
   // hosts
-  [/clients3\.google\.com/].forEach(headerTester('host'));
+  [
+    /clients3\.google\.com/,
+    /captive\.apple\.com/
+  ].forEach(headerTester('host'));
 
   return ok 
 }
